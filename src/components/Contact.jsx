bca94cb0381eb8368c752ecb391015c1d17d8c06
AmeyTari2002 <ameytari2002@gmail.com>
2024-02-12 19:50:38 +0530

    hello

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,22 +21,22 @@ const Contact = () => {
                             <input type="text" name="name" required
                                 placeholder='Enter your name'
                                 className='p-2 bg-transparent  
-                        border-2 rounded-md text-white 
+                        border-2 rounded-md 
                         focus:outline-none'
                             />
                             <input type="email" name="email" required
                                 placeholder='Enter your Email'
                                 className='my-2 p-2 bg-transparent  
-                        border-2 rounded-md text-white 
+                        border-2 rounded-md  
                         focus:outline-none required'
                             />
                             <textarea name="message" required
                                 placeholder='Enter yur Message'
                                 rows="6"
                                 className='p-2 bg-transparent border-2 rounded-md 
-                         text-white focus:outline-none '>
+                         focus:outline-none '>
                             </textarea>
-                            <button className='text-white bg-gradient-to-b
+                            <button className='bg-gradient-to-b
                      from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex
                      items-center rounded-md hover:scale-110 duration-300 '>
                                 Let's talk
